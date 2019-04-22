44bf4aa149ae64a6d498ed769711c4ab35069679
Antonio Fernandes <fernandes.anttonio@gmail.com>
2019-04-22 14:34:32 -0300

    Lista III

---
+++ Lista_3.R
@@ -0,0 +1,21 @@
+##[AD-2019]
+##LISTA III
+##ANTÔNIO FERNANDES
+
+##Q.2
+
+  ##Declarando duas variáveis
+
+x <- 4 #x igual a 4
+y <- 3 #y igual a 3
+
+  ##Soma de x e Y atribuído a Z
+
+z <- x + y #z é igual a x+y
+
+  ##multiplicando z pelo cpf
+
+a <- z*08819478498 #a é igual ao resultado da multiplicação de z pelo cpf
+
+
+
